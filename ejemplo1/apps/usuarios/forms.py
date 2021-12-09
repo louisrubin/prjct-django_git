53207badce94bf3928bb3219876dd9bd5677d99b
louisrubin <miguel10.rbn@gmail.com>
2021-12-09 17:08:00 -0300

    includes -> forms errors

--- ejemplo1/apps/usuarios/forms.py
+++ ejemplo1/apps/usuarios/forms.py
@@ -3,6 +3,8 @@ from django import forms
 from .models import Usuario
 
 class ProductoForm(forms.ModelForm):
+    nombre = forms.CharField(label="Nombre de Usuario", widget=forms.TextInput(attrs={'class':'form-control'}))
+
     class Meta:
         model = Usuario
         fields = ["username", "first_name", "dni"]
